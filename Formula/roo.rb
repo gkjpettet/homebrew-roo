2f959c8caadf05ac38e5d3c94cc63cfc63397859
Garry Pettet <contact@garrypettet.com>
2019-04-09 14:04:03 +0100

    Version 3.0.2

--- Formula/roo.rb
+++ Formula/roo.rb
@@ -1,8 +1,8 @@
 class Roo < Formula
   desc "The Roo programming language"
   homepage "https://roolang.org"
-  url "https://github.com/gkjpettet/roo/files/3048208/roo-3.0.1.61-macos.zip"
-  sha256 "e55e5f83102eab19dd47a9bb7259ff1323ec2286e1ac6c568fe796b50c4f3f28" # openssl sha256 [file]
+  url "https://github.com/gkjpettet/roo/files/3059059/roo-3.0.2.73-macos.zip"
+  sha256 "df22e9bd216ece29d09406cffe7d75609ce8a0cab31f6735a669afc48b268b02" # openssl sha256 [file]
 
   def install
     bin.install "roo Libs"
